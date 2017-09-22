b48a94cecb9de39e93aec2baacf39011ca54bee1
Carmen Peralta <carmenilse@IPs-MacBook-Pro.local>
2017-09-22 17:06:14 -0400

    wikistack

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const wikiApp = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const nunjucks = require('nunjucks');
+const path = require('path');
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+wikiApp.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+wikiApp.engine('html', nunjucks.render);
+
+//using morgan to log status
+wikiApp.use(morgan('dev'));
+
+//wikiApp.use(routes);
+wikiApp.get('/', function(req, res) {
+  res.send("whatever");
+  console.log("listen");
+});
+
+
+
+wikiApp.listen(3000, function() {
+  console.log("Listening on port");
+})
